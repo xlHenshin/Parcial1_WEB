c1685baf8ecc9460b6790f1b08743f193aa346e5
xlHenshin <andreslayer117@gmail.com>
2021-09-19 23:25:20 -0500

    Responsive Menu done

--- js/index.js
+++ js/index.js
@@ -14,3 +14,15 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
   },
 });
+
+const buttonMenu = document.getElementById("openMenu");
+const menu = document.getElementById("menu");
+const closeMenu = document.getElementById("close");
+
+buttonMenu.addEventListener("click", e=>{
+    menu.classList.add("menu__container--mobile");
+})
+
+closeMenu.addEventListener("click", e=>{
+    menu.classList.remove("menu__container--mobile");
+})
