3ef9c96fc4f609502e75771a37bec4054a81b575
xlHenshin <andreslayer117@gmail.com>
2021-10-29 01:45:51 -0500

    Order by

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const products = [
     {
         id: 1,
         name: "Wall Connector - 24 ft",
-        price: "$500",
+        price: 500,
         image: "https://www.tesla.com/ns_videos/commerce/content/dam/tesla/CAR_ACCESSORIES/MODEL_S/CHARGING_ADAPTERS/1457768-01-F_0.jpg",
         description: "Wall Connector is the most convenient charging solution for houses, apartments, hospitality properties and workplaces.",
         type: "charging"
@@ -10,7 +10,7 @@ const products = [
     {
         id: 2,
         name: "Model S Rack Roof",
-        price: "$450",
+        price: 450,
         image: "https://www.tesla.com/ns_videos/commerce/content/dam/tesla/CAR_ACCESSORIES/MODEL_S/EXTERIOR/1494684-00-A_1.jpg",
         description: "Model S Roof Rack – Glass Roof was designed and engineered from the ground up for maximum aerodynamic efficiency, minimal interior noise and impact to range.",
         type: "vAccesories"
@@ -18,7 +18,7 @@ const products = [
     {
         id: 3,
         name: "Stainless Steel Water Bottle",
-        price: "$30",
+        price: 30,
         image: "https://www.tesla.com/ns_videos/commerce/content/dam/tesla/LIFESTYLE/ACCESSORIES_COLLECTIBLES/DRINKWARE/100045802_0.jpg",
         description: "The Tesla Stainless Steel Water Bottle is a conveniently sized, insulated water bottle that holds 600mL / 20fl oz of liquid. It is BPA free, vacuum insulated, double walled, 18/8 stainless steel and perfect for cold or hot liquids.",
         type: "lifestyle"
@@ -26,7 +26,7 @@ const products = [
     {
         id: 4,
         name: "Men's Corp Jacket",
-        price: "$90",
+        price: 90,
         image: "https://www.tesla.com/ns_videos/commerce/content/dam/tesla/LIFESTYLE/OUTERWEAR/JACKETS/5645685-00-A_0.jpg",
         description: "Fully customized and uniquely styled, the Men's Corp Jacket features a silicone-printed T logo on the left chest and prominent Tesla wordmark across the back.",
         type: "menClothing"
@@ -34,7 +34,7 @@ const products = [
     {
         id: 5,
         name: "Women's Zip-Up Track Hoodie",
-        price: "$60",
+        price: 60,
         image: "https://www.tesla.com/ns_videos/commerce/content/dam/tesla/LIFESTYLE/ACTIVEWEAR/TRAINING/5645669-00-A_0.jpg",
         description: "Featuring soft fleece and a jersey-lined hood, the Women’s Zip-Up Track Hoodie delivers the ultimate coverage and comfort in a standard fit. Made with two layers of soft cotton with a polyester yarn in interlock stitch, the light and comfortable fabric is ideal for both training and leisure.",
         type: "womenClothing"
@@ -42,7 +42,7 @@ const products = [
     {
         id: 6,
         name: "Corded Mobile Connector",
-        price: "$520",
+        price: 520,
         image: "https://www.tesla.com/ns_videos/commerce/content/dam/tesla/CAR_ACCESSORIES/MODEL_X/CHARGING_ADAPTERS/1106293-00-A_0.jpg",
         description: "The Corded Mobile Connector features a NEMA 14-50 plug and improved charging speeds in comparison to the Gen 2 Mobile Connector with NEMA 5-15 Adapter. ",
         type: "charging"
@@ -80,7 +80,7 @@ const productTemplate = (item) =>{
                 <div class="product__description">
                     <div class="product__text">
                         <h3 class="product__name">${item.name}</h3>
-                        <h3 class="product__price">${item.price}</h3>
+                        <h3 class="product__price">$${item.price}</h3>
                     </div>
                     ${buttonHtml}
                 </div>
@@ -107,11 +107,24 @@ products.forEach(product =>{
     productTemplate(product);
 });
 
-const filterByCategory = document.getElementById("categories");
+const filterByCategorySelect = document.getElementById("categories");
+const orderBySelect = document.getElementById("orderBy");
 
-filterByCategory.addEventListener("change", e=>{
+orderBySelect.addEventListener("change", e =>{
 
-    const category = filterByCategory.value;
+    loadProducts();
+});
+
+filterByCategorySelect.addEventListener("change", e=>{
+
+    loadProducts();
+});
+
+const loadProducts = ()=>{
+
+    const category = filterByCategorySelect.value;
+    const order = orderBySelect.value;
+
     productSection.innerHTML = "";
     let filteredProductsByCategory;
 
@@ -120,9 +133,17 @@ filterByCategory.addEventListener("change", e=>{
     }else{
         filteredProductsByCategory = products;
     }
-    
+
+    if (order ==="asc") {
+        filteredProductsByCategory = filteredProductsByCategory.sort((a,b)=>a.price - b.price);
+    }
+
+    if (order ==="desc") {
+        filteredProductsByCategory = filteredProductsByCategory.sort((a,b)=>b.price - a.price);
+    }
+
     filteredProductsByCategory.forEach(product =>{
         
         productTemplate(product);
     });
-})
+}
